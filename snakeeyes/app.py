554d102502d319a5161f255c26d42ed957cec577
Quan Hua <quanhua92@gmail.com>
2018-07-06 21:19:35 +0700

    feat: add page blueprints

--- snakeeyes/app.py
+++ snakeeyes/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+from snakeeyes.blueprints.page import page
+
 def create_app():
   """Create a Flask app using the app factor pattern.
   :return: Flask app
@@ -9,12 +11,6 @@ def create_app():
   app.config.from_object('config.settings')
   app.config.from_pyfile('settings.py', silent=True)
 
-  @app.route("/")
-  def index():
-    """
-    Render a Hello World response.
-    :return: Flask response
-    """
-    return app.config['HELLO']
+  app.register_blueprint(page)
 
   return app
